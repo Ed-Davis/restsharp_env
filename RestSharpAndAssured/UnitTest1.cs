dac01a8d324f03d0984e3acbab5a31aba50e673c
Ed-Davis <skootaman@btopenworld.com>
2024-11-26 19:33:40 +0000

    Initial Environment

---
+++ RestSharpAndAssured/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace RestSharpAndAssured;
+
+public class Program
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        
+        Assert.Pass();
+    }
+}
